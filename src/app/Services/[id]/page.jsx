098274be2794477c02a3ec6fafec36407261affb
Ashik <ashikkoirala68@gmail.com>
2024-06-12 20:57:37 +0545

    solving the module error

--- src/app/Services/[id]/page.jsx
+++ src/app/Services/[id]/page.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React from 'react';
-import Services from "../../../../public/services.json";
+import Services from "../../../../public/Services.json";
 import { usePathname } from 'next/navigation';
 import { useParams } from 'next/navigation';
 import Image from 'next/image';
